5cbc82d96a42c2dcaec6c1beff9d7211ccd314ed
Youngjun Kim <zkj9404@naver.com>
2024-06-23 22:34:01 +0900

    posts[id] 페이지 시작

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,3 +1,64 @@
-export default function Page() {
-  return <div className='text-white text-4xl'>life page</div>;
+import db from '@/lib/db';
+import { formatToTimeAgo } from '@/lib/utils';
+import { ChatBubbleBottomCenterIcon, HandThumbUpIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      /** _count를 하면 항목이 가리키는 개수를 알 수 있음.  */
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+  return posts;
+}
+
+export const metadata = {
+  title: '동네생활',
+};
+
+export default async function Page() {
+  const posts = await getPosts();
+  console.log(posts);
+  return (
+    <div className='p-5 flex flex-col '>
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className='pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex flex-col last:pb-0 last:border-b-0 gap-2'
+        >
+          <h2 className='text-white text-lg font-semibold'>{post.title}</h2>
+          <p className='mb-2'>{post.description}</p>
+          <div className='flex items-center justify-between text-sm'>
+            <div className='flex gap-4 items-center'>
+              <span>{formatToTimeAgo(post.created_at)}</span>
+              <span> - </span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className='flex gap-4 items-center *:flex *:gap-1 *:items-center'>
+              <span className=''>
+                <HandThumbUpIcon className='size-4' />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className='size-4' />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
